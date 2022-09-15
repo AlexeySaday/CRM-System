087cb217129efacc3c7e58e19399b678a86adf09
Alex Saday <alexeykildyushev@yandex.ru>
2022-09-15 10:09:42 +0300

    'begging'

---
+++ CRM System/Message.cs
@@ -0,0 +1,15 @@
+﻿namespace CRM_System
+{
+    public class Message
+    {
+        public string Id { get;}
+        public string Name { get; set; }
+        public string MessageText { get; set; }
+        public Message(string name, string messageText)
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = name;
+            MessageText = messageText;
+        }
+    }
+}
---
+++ CRM System/Program.cs
@@ -0,0 +1,36 @@
+using CRM_System;
+
+var builder = WebApplication.CreateBuilder(args); 
+var app = builder.Build();
+
+ 
+app.UseStaticFiles();
+app.UseDefaultFiles();
+
+List<Message> messages = new List<Message>()
+{
+    new("Игорь Михайлович","Оставить заявку"),
+    new("Федор Петрович","Организовать всиречу")
+};
+app.MapGet("/api/messages", () => messages);
+app.MapGet("/api/messages/{id}",(string id) =>
+{
+    Message? message = messages.Where(x => x.Id == id).FirstOrDefault();
+    if (message != null) return Results.NotFound("Не получилось найти данное сообщение");
+    return Results.Json(message);
+});
+app.MapPost("/api/messages", (string name, string message) =>
+{
+    messages.Add(new(name, message));
+    return Results.Ok();
+});
+ 
+app.MapPut("/api/messages", (Message message) =>
+{
+    var m = messages.Where(e => e.Id == message.Id).FirstOrDefault();
+    if (m == null) return Results.NotFound("Не найденно");
+    m.Name = message.Name;
+    m.MessageText = message.MessageText;
+    return Results.Ok();
+});
+app.Run();
