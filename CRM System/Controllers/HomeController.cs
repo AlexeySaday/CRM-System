ae51e9e1e2dbc144fa76ef4ce96d7572155e881e
Alexey Kildyushev <102044544+AlexeySaday@users.noreply.github.com>
2022-09-15 13:19:52 +0300

    Update HomeController.cs

--- CRM System/Controllers/HomeController.cs
+++ CRM System/Controllers/HomeController.cs
@@ -4,9 +4,15 @@ namespace CRM_System.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
         public IActionResult Index()
         {
             return View();
         }
     }
+    
 }
